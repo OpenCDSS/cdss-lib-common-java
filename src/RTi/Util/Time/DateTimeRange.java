2f0818fdb4f1a433fe4a2ac3048a500dcdb1bc51
Steve Malers <steve.malers@openwaterfoundation.org>
2008-04-04 17:59:44 +0000

    Add DateTimeRange to help with period processing.
    git-svn-id: file:///cygdrive/C/owf-svnrepos/RTi_Common/trunk@114 81c4e16f-9576-dd43-849c-dbad87f9bafb

---
+++ src/RTi/Util/Time/DateTimeRange.java
@@ -0,0 +1,50 @@
+package RTi.Util.Time;
+
+/**
+This class stores a range defined by two DateTime end-points.
+It is useful for specifying a processing period.
+Currently the instance is immutable and copies of the DateTime data are copied
+at construction.
+Currently there is no validation done.
+*/
+public class DateTimeRange
+{
+    /**
+     * Starting DateTime for the range.
+     */
+    private DateTime __start = null;
+    
+    /**
+     * Ending DateTime for the range.
+     */
+    private DateTime __end = null;
+    
+    /**
+     * Constructor.
+     * @param start starting value in the range.  Can be null to indicate open-ended
+     * range (from available start).
+     * @param end ending value in the range.  Can be null to indicate open-ended
+     * range (to available end).
+     */
+    public DateTimeRange ( DateTime start, DateTime end )
+    {
+        __start = new DateTime ( start );
+        __end = new DateTime ( end );
+    }
+    
+    /**
+     * Return the ending date/time in the range (can be null) if open-ended.
+     */
+    public DateTime getEnd ()
+    {
+        return __end;
+    }
+    
+    /**
+     * Return the starting date/time in the range (can be null) if open-ended.
+     */
+    public DateTime getStart ()
+    {
+        return __start;
+    }
+}
