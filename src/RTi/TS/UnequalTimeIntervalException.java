b987111bdc0c81d94a02cd2261db186a6cc200e5
Steve Malers <steve.malers@openwaterfoundation.org>
2008-11-19 23:23:56 +0000

    Add UnequalTimeIntervalException for more specific error handling.
    git-svn-id: file:///cygdrive/C/owf-svnrepos/RTi_Common/trunk@188 81c4e16f-9576-dd43-849c-dbad87f9bafb

---
+++ src/RTi/TS/UnequalTimeIntervalException.java
@@ -0,0 +1,26 @@
+package RTi.TS;
+
+/**
+This exception should be thrown when a method requires that time intervals be equal for processing.
+*/
+public class UnequalTimeIntervalException extends RuntimeException
+{
+
+/**
+Construct with a string message.
+@param s String message.
+*/
+public UnequalTimeIntervalException( String s )
+{	super ( s );
+}
+
+/**
+Finalize before garbage collection.
+@exception Throwable if an error occurs.
+*/
+protected void finalize()
+throws Throwable
+{	super.finalize();
+}
+
+}
