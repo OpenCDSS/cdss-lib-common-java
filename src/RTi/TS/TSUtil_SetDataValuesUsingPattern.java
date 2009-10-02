ee8fac2f689baa68aa8862c1a2da3935fd051d94
Steve Malers <steve.malers@openwaterfoundation.org>
2009-10-02 22:33:33 +0000

    Allow missing values to be specified as blanks when creating new pattern time series.
    git-svn-id: file:///cygdrive/C/owf-svnrepos/RTi_Common/trunk@334 81c4e16f-9576-dd43-849c-dbad87f9bafb

--- src/RTi/TS/TSUtil_SetDataValuesUsingPattern.java
+++ src/RTi/TS/TSUtil_SetDataValuesUsingPattern.java
@@ -22,16 +22,30 @@ Set the time series data to a repeating sequence of values.
 @param ts Time series to update.
 @param startDate Date to start assignment.
 @param endDate Date to stop assignment.
-@param patternValues Data values to set as time series data.
+@param patternValues Data values to set as time series data.  NaN will cause the value to be set to the
+missing data value for the time series.
 @param patternFlags String flag values to set as data, or null to not use flags.
 */
 public void setDataValuesUsingPattern (	TS ts, DateTime startDate,
-					DateTime endDate, double [] patternValues, String [] patternFlags )
+	DateTime endDate, double [] patternValues, String [] patternFlags )
 throws Exception
 {	// Get valid dates because the ones passed in may have been null...
 	TSLimits valid_dates = TSUtil.getValidPeriod ( ts, startDate, endDate );
 	DateTime start = valid_dates.getDate1();
 	DateTime end = valid_dates.getDate2();
+	
+	// Replace NaN values in patternValues with the missing value for the time series.  Do it
+	// here so that checks are minimized and the genesis information shows values relevant for
+	// the time series.
+	double [] patternValues2 = new double[patternValues.length];
+	for ( int i = 0; i < patternValues.length; i++ ) {
+        if ( Double.isNaN(patternValues[i]) ) {
+            patternValues2[i] = ts.getMissing();
+        }
+        else {
+            patternValues2[i] = patternValues[i];
+        }
+	}
 
 	TSIterator tsi = ts.iterator ( startDate, endDate );
 	int ipattern = 0;
@@ -43,12 +57,12 @@ throws Exception
 	    // next() will return null when the last date in the processing period has been passed.
 		date = tsi.getDate();
 		if ( usingDataFlags ) {
-		    ts.setDataValue ( date, patternValues[ipattern++], patternFlags[iflag++], 0 );
+		    ts.setDataValue ( date, patternValues2[ipattern++], patternFlags[iflag++], 0 );
 		}
 		else {
-		    ts.setDataValue ( date, patternValues[ipattern++] );
+		    ts.setDataValue ( date, patternValues2[ipattern++] );
 	    }
-		if ( ipattern == patternValues.length ) {
+		if ( ipattern == patternValues2.length ) {
 			// Reset to start at the beginning of the pattern...
 			ipattern = 0;
 		}
@@ -60,11 +74,11 @@ throws Exception
 	// Set the genesis information...
 	ts.setDescription ( ts.getDescription() + ", pattern" );
 	StringBuffer patternbuf = new StringBuffer ();
-	for ( int i = 0; i < patternValues.length; i++ ) {
+	for ( int i = 0; i < patternValues2.length; i++ ) {
 		if ( i != 0 ) {
 			patternbuf.append ( ",");
 		}
-		patternbuf.append ( StringUtil.formatString(patternValues[i],"%.3f") );
+		patternbuf.append ( StringUtil.formatString(patternValues2[i],"%.3f") );
 	}
     StringBuffer flagbuf = new StringBuffer ();
     if ( usingDataFlags ) {
@@ -78,9 +92,8 @@ throws Exception
             flagbuf.insert(0, ", flags=" );
         }
     }
-	ts.addToGenesis ( "Set " + start.toString() + " to " +
-	end.toString() + " to pattern=" + patternbuf.toString() + flagbuf.toString());
-
+	ts.addToGenesis ( "Set " + start + " to " +
+	end + " to pattern=" + patternbuf.toString() + flagbuf.toString());
 }
 
 }
