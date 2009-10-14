1514e2ccf29ddf55879bddd1302c428c6488d8e7
Steve Malers <steve.malers@openwaterfoundation.org>
2009-10-14 17:27:23 +0000

    Change class to enum to make the statistics types safer to use.
    git-svn-id: file:///cygdrive/C/owf-svnrepos/RTi_Common/trunk@343 81c4e16f-9576-dd43-849c-dbad87f9bafb

--- src/RTi/TS/TSStatistic.java
+++ src/RTi/TS/TSStatistic.java
@@ -12,41 +12,193 @@ from the sample given by the time series data points.  Some statistics are gener
 nature and could be applied outside of time series.  Some are specific to time series
 (e.g., day of year that some condition occurs).
 */
-public class TSStatistic
+public enum TSStatistic
 {
-
-/**
-Statistics that are available for analysis.
-*/
-public final static String
-    Count = "Count", // Count of missing and non-missing values
-	CountGE = "CountGE", // Count of values >= test
-	CountGT = "CountGT", // Count of values > test
-	CountLE = "CountLE", // Count of values <= test
-	CountLT = "CountLT", // Count of values < test
-	DayOfFirstGE = "DayOfFirstGE", // Day of first value >= test
-	DayOfFirstGT = "DayOfFirstGT", // Day of first value > test
-	DayOfFirstLE = "DayOfFirstLE", // Day of first value <= test
-	DayOfFirstLT = "DayOfFirstLT", // Day of first value < test
-	DayOfLastGE = "DayOfLastGE", // Day of last value >= test
-	DayOfLastGT = "DayOfLastGT", // Day of last value > test
-	DayOfLastLE = "DayOfLastLE", // Day of last value <= test
-	DayOfLastLT = "DayOfLastLT", // Day of last value < test
-	DayOfMax = "DayOfMax", // Day of maximum value
-	DayOfMin = "DayOfMin", // Day of minimum value
-    ExceedanceProbabilityGE10 = "ExceedanceProbabilityGE10", // Probability of exceeding value is >= 10%
-    ExceedanceProbabilityGE50 = "ExceedanceProbabilityGE50", // Probability of exceeding value is >= 50%
-    ExceedanceProbabilityGE90 = "ExceedanceProbabilityGE90", // Probability of exceeding value is >= 90%
-	Max = "Max", // Maximum value in the sample
-	Median = "Median", // Median value in the sample
-	Mean = "Mean", // Mean value in the sample
-	Min = "Min", // Minimum value in the sample
-    MissingCount = "MissingCount", // Count of missing values
-    MissingPercent = "MissingPercent", // Percent of missing values
-    NonmissingCount = "NonmissingCount", // Count of non-missing values
-    NonmissingPercent = "NonmissingPercent", // Percent of non-missing values
-    NqYY = "NqYY", // for daily data: 10-year recurrence interval for lowest 7-day average flow (each year)
-	Total = "Total"; // Total of values in the sample
+    /**
+     * Count of missing and non-missing values (total count).
+     */
+    COUNT("Count"),
+    /**
+     * Count of values >= test value.
+     */
+	COUNT_GE ( "CountGE" ),
+	/**
+	 * Count of values > test value.
+	 */
+	COUNT_GT ( "CountGT" ),
+	/**
+	 * Count of values <= test value.
+	 */
+	COUNT_LE ( "CountLE" ),
+	/**
+	 * Count of values < test value.
+	 */
+	COUNT_LT ( "CountLT" ),
+	/**
+	 * Day of first value >= test value.
+	 */
+	DAY_OF_FIRST_GE ( "DayOfFirstGE" ),
+	/**
+	 * Day of first value > test value.
+	 */
+	DAY_OF_FIRST_GT ( "DayOfFirstGT" ),
+	/**
+	 * Day of first value <= test value.
+	 */
+	DAY_OF_FIRST_LE ( "DayOfFirstLE" ),
+	/**
+	 * Day of first value < test value.
+	 */
+	DAY_OF_FIRST_LT ( "DayOfFirstLT" ),
+	/**
+	 * Day of last value >= test value.
+	 */
+	DAY_OF_LAST_GE ( "DayOfLastGE" ),
+	/**
+	 * Day of last value > test value.
+	 */
+	DAY_OF_LAST_GT ( "DayOfLastGT" ),
+	/**
+	 * Day of last value <= test value.
+	 */
+	DAY_OF_LAST_LE ( "DayOfLastLE" ),
+	/**
+	 * Day of last value < test value.
+	 */
+	DAY_OF_LAST_LT ( "DayOfLastLT" ),
+	/**
+	 * Day of maximum value.
+	 */
+	DAY_OF_MAX ( "DayOfMax" ),
+	/**
+	 * Day of minimum value.
+	 */
+	DAY_OF_MIN ( "DayOfMin" ),
+    /**
+     * Maximum of number of consecutive intervals with values below the mean.
+     */
+	DEFICIT_LENGTH_MAX ( "DeficitLengthMax" ),
+    /**
+     * Mean of number of consecutive intervals with values below the mean.
+     */
+    DEFICIT_LENGTH_MEAN ( "DeficitLengthMean" ),
+    /**
+     * Mean of number of consecutive intervals with values below the mean.
+     */
+    DEFICIT_LENGTH_MIN ( "DeficitLengthMin" ),
+    /**
+     * Maximum of (mean - value) sum when consecutive values are below the mean.
+     */
+    DEFICIT_MAX ( "DeficitMax" ),
+    /**
+     * Mean of (mean - value) sum when consecutive values are above the mean.
+     */
+    DEFICIT_MEAN ( "DeficitMean" ),
+    /**
+     * Minimum of (mean - value) sum when consecutive values are above the mean.
+     */
+    DEFICIT_MIN ( "DeficitMin" ),
+	/**
+	 * Probability of exceeding value is >= 10%.
+	 */
+    EXCEEDANCE_PROBABILITY_GE10 ( "ExceedanceProbabilityGE10" ),
+    /**
+     * Probability of exceeding value is >= 50%.
+     */
+    EXCEEDANCE_PROBABILITY_GE50 ( "ExceedanceProbabilityGE50" ),
+    /**
+     * Probability of exceeding value is >= 90%.
+     */
+    EXCEEDANCE_PROBABILITY_GE90 ( "ExceedanceProbabilityGE90" ),
+    /**
+     * Maximum value in the sample.
+     */
+	MAX ( "Max" ),
+	/**
+	 * Median value in the sample.
+	 */
+	MEDIAN ( "Median" ),
+	/**
+	 * Mean value in the sample.
+	 */
+	MEAN ( "Mean" ),
+	/**
+	 * Minimum value in the sample.
+	 */
+	MIN ( "Min" ),
+	/**
+	 * Count of missing values in the sample.
+	 */
+    MISSING_COUNT ( "MissingCount" ),
+    /**
+     * Percent of missing values in the sample.
+     */
+    MISSING_PERCENT ( "MissingPercent" ),
+    /**
+     * Count of non-missing values in the sample.
+     */
+    NONMISSING_COUNT ( "NonmissingCount" ),
+    /**
+     * Percent of non-missing values.
+     */
+    NONMISSING_PERCENT ( "NonmissingPercent" ),
+    /**
+     * For daily data: 10-year recurrence interval for lowest 7-day average flow (each year) is 7q10
+     */
+    NQYY ( "NqYY" ),
+    /**
+     * Coefficient of skew.
+     */
+    SKEW ( "Skew" ),
+    /**
+     * Standard deviation.
+     */
+    STD_DEV ( "StdDev" ),
+    /**
+     * Maximum of number of consecutive intervals with values above the mean.
+     */
+    SURPLUS_LENGTH_MAX ( "SurplusLengthMax" ),
+    /**
+     * Mean of number of consecutive intervals with values above the mean.
+     */
+    SURPLUS_LENGTH_MEAN ( "SurplusLengthMean" ),
+    /**
+     * Mean of number of consecutive intervals with values below the mean.
+     */
+    SURPLUS_LENGTH_MIN ( "SurplusLengthMin" ),
+    /**
+     * Maximum of value sum when consecutive values are above the mean.
+     */
+    SURPLUS_MAX ( "SurplusMax" ),
+    /**
+     * Mean of value sum when consecutive values are above the mean.
+     */
+    SURPLUS_MEAN ( "SurplusMean" ),
+    /**
+     * Minimum of value sum when consecutive values are above the mean.
+     */
+    SURPLUS_MIN ( "SurplusMin" ),
+    /**
+     * Total of values in the sample.
+     */
+	TOTAL ( "Total" ),
+	/**
+	 * Sample variance.
+	 */
+	VARIANCE ( "Variance" );
+    
+    /**
+     * The name that should be displayed when the best fit type is used in UIs and reports.
+     */
+    private final String displayName;
+    
+    /**
+     * Construct a time series statistic enumeration value.
+     * @param displayName name that should be displayed in choices, etc.
+     */
+    private TSStatistic(String displayName) {
+        this.displayName = displayName;
+    }
 
 // TODO SAM 2005-09-30
 // Need to add:
@@ -74,29 +226,31 @@ choices (these features will be phased in over time as statistics are added).
 statistic (e.g., average value for the year).  Pass TimeInterval.UNKNOWN to get all choices.
 @param timescale MeasTimeScale.ACCM, etc., indicating whether the statistic is
 expected on accumulated, mean, instantaneous data.  Pass null to get all choices.
+@deprecated due to the large number of statistics, choices should be determined for the specific
+computational code to avoid inappropriate choices.
 */
 public static List getStatisticChoicesForInterval ( int interval, String timescale )
 {	List statistics = new Vector();
 	if ( (interval >= TimeInterval.MONTH) || (interval == TimeInterval.UNKNOWN) ) {
-		statistics.add ( CountGE );
-		statistics.add ( CountGT );
-		statistics.add ( CountLE );
-		statistics.add ( CountLT );
-		statistics.add ( DayOfFirstGE );
-		statistics.add ( DayOfFirstGT );
-		statistics.add ( DayOfFirstLE );
-		statistics.add ( DayOfFirstLT );
-		statistics.add ( DayOfLastGE );
-		statistics.add ( DayOfLastGT );
-		statistics.add ( DayOfLastLE );
-		statistics.add ( DayOfLastLT );
-		statistics.add ( DayOfMax );
-		statistics.add ( DayOfMin );
-		statistics.add ( Max );
-		statistics.add ( Median );
-		statistics.add ( Mean );
-		statistics.add ( Min );
-		statistics.add ( Total );
+		statistics.add ( COUNT_GE );
+		statistics.add ( COUNT_GT );
+		statistics.add ( COUNT_LE );
+		statistics.add ( COUNT_LT );
+		statistics.add ( DAY_OF_FIRST_GE );
+		statistics.add ( DAY_OF_FIRST_GT );
+		statistics.add ( DAY_OF_FIRST_LE );
+		statistics.add ( DAY_OF_FIRST_LT );
+		statistics.add ( DAY_OF_LAST_GE );
+		statistics.add ( DAY_OF_LAST_GT );
+		statistics.add ( DAY_OF_LAST_LE );
+		statistics.add ( DAY_OF_LAST_LT );
+		statistics.add ( DAY_OF_MAX );
+		statistics.add ( DAY_OF_MIN );
+		statistics.add ( MAX );
+		statistics.add ( MEDIAN );
+		statistics.add ( MEAN );
+		statistics.add ( MIN );
+		statistics.add ( TOTAL );
 	}
 	return statistics;
 }
@@ -114,6 +268,8 @@ choices (these features will be phased in over time as statistics are added).
 statistic (e.g., average value for the year).  Pass TimeInterval.UNKNOWN to get all choices.
 @param timescale MeasTimeScale.ACCM, etc., indicating whether the statistic is
 expected on accumulated, mean, instantaneous data.  Pass null to get all choices.
+@deprecated due to the large number of statistics, choices should be determined for the specific
+computational code to avoid inappropriate choices.
 */
 public static List getStatisticChoicesForSimpleSample ( int interval, String timescale )
 {	List statistics = new Vector();
@@ -122,8 +278,8 @@ public static List getStatisticChoicesForSimpleSample ( int interval, String tim
     //statistics.addElement ( ExceedanceProbabilityGE10 );
     //statistics.addElement ( ExceedanceProbabilityGE50 );
     //statistics.addElement ( ExceedanceProbabilityGE90 );
-    statistics.add ( Mean );
-    statistics.add ( Median );
+    statistics.add ( MEAN );
+    statistics.add ( MEDIAN );
 	//statistics.addElement ( Min );
 	return statistics;
 }
@@ -143,6 +299,31 @@ public static Class getStatisticDataType ( String statistic )
     else {
         return Double.class;
     }
+}
+
+/**
+ * Return the display name for the statistic.  This is usually the same as the
+ * value but using appropriate mixed case.
+ * @return the display name.
+ */
+@Override
+public String toString() {
+    return displayName;
+}
+
+/**
+ * Return the enumeration value given a string name (case-independent).
+ * @return the enumeration value given a string name (case-independent), or null if not matched.
+ */
+public static TSStatistic valueOfIgnoreCase(String name)
+{
+    TSStatistic [] values = values();
+    for ( TSStatistic t : values ) {
+        if ( name.equalsIgnoreCase(t.toString()) ) {
+            return t;
+        }
+    } 
+    return null;
 }
 
 }
