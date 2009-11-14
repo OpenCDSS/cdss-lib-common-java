d04dda35e8d8f995e9b683a563528fc4afc317bb
Steve Malers <steve.malers@openwaterfoundation.org>
2009-11-14 00:00:19 +0000

    Clean up toString() to work more explicitly with other code.  Adjust the day choices based on the month that is selected.
    git-svn-id: file:///cygdrive/C/owf-svnrepos/RTi_Common/trunk@367 81c4e16f-9576-dd43-849c-dbad87f9bafb

--- src/RTi/Util/Time/DateTime_JPanel.java
+++ src/RTi/Util/Time/DateTime_JPanel.java
@@ -1,6 +1,7 @@
 package RTi.Util.Time;
 
 import java.awt.Color;
+import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
@@ -10,6 +11,7 @@ import javax.swing.BorderFactory;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 
+import java.util.List;
 import java.util.Vector;
 
 import RTi.Util.GUI.JGUIUtil;
@@ -19,7 +21,7 @@ import RTi.Util.String.StringUtil;
 /**
 A JPanel to edit a DateTime.
 */
-public class DateTime_JPanel extends JPanel
+public class DateTime_JPanel extends JPanel implements ActionListener
 {
 
 /**
@@ -111,6 +113,20 @@ public DateTime_JPanel ( String title, int interval_max, int interval_min, DateT
 }
 
 /**
+Handle action events.
+@param event action event to handle.
+*/
+@Override
+public void actionPerformed(ActionEvent event)
+{
+    // If the event source is the month choice, update the day choices to be correct.
+    if ( event.getSource() == __month_JComboBox ) {
+        setDayChoices ( __year_JComboBox, __month_JComboBox, __day_JComboBox );
+    }
+    
+}
+
+/**
 Add an action listener for all of the components.  The listeners will be notified if
 any action events occur.
 */
@@ -196,6 +212,46 @@ public void setDateTime ( DateTime dt )
 }
 
 /**
+Set up the daily choices based on the selected year.  If the year is not visible and the month is 2,
+include 29 days.
+*/
+private void setDayChoices ( SimpleJComboBox year_JComboBox, SimpleJComboBox month_JComboBox,
+    SimpleJComboBox day_JComboBox )
+{
+    List<String> day_Vector = new Vector();
+    day_Vector.add ( "" );    // Select to ignore this information
+    int maxDay = 31;
+    int year = -9999;
+    int month = -9999;
+    if ( year_JComboBox.isVisible() ) {
+        String yearSelection = year_JComboBox.getSelected().trim();
+        if ( StringUtil.isInteger(yearSelection) ) {
+            year = Integer.parseInt(yearSelection);
+        }
+    }
+    if ( month_JComboBox.isVisible() ) {
+        String monthSelection = month_JComboBox.getSelected().trim();
+        if ( StringUtil.isInteger(monthSelection) ) {
+            month = Integer.parseInt(monthSelection);
+        }
+    }
+    if ( month != -9999 ) {
+        // Try to be more specific with the number of days.
+        if ( year != -9999 ) {
+            maxDay = TimeUtil.numDaysInMonth(month, year);
+        }
+        else {
+            // Assume a leap year...
+            maxDay = TimeUtil.numDaysInMonth(month, 1976);
+        }
+    }
+    for ( int i = 1; i <= maxDay; i++ ) {
+        day_Vector.add( "" + i );
+    }
+    day_JComboBox.setData(day_Vector);
+}
+
+/**
 Set the panel enabled/disabled by setting each component enabled/disabled.
 @param enabled Indicates whether the panel components should be enabled or disabled.
 */
@@ -265,10 +321,10 @@ private void setupUI ()
     int y_label = 0;    // Positions of labels and entry fields.
     int y_entry = 1;
     // Add the year...
-    Vector year_Vector = new Vector();
-    year_Vector.addElement ( "" );
+    List<String> year_Vector = new Vector();
+    year_Vector.add( "" );
     for ( int i = 1900; i <= 2100; i++ ) {
-        year_Vector.addElement ( "" + i );
+        year_Vector.add( "" + i );
     }
     // Allow edits because may not have all years of interest in the list
     __year_JComboBox = new SimpleJComboBox ( year_Vector, true );
@@ -279,12 +335,13 @@ private void setupUI ()
         x++, y_entry, 1, 1, 1, 0, insetsTLBR, GridBagConstraints.NONE, GridBagConstraints.WEST);
 
     // Add the month...
-    Vector month_Vector = new Vector();
-    month_Vector.addElement ( "" );    // Select to ignore this information
+    List<String> month_Vector = new Vector();
+    month_Vector.add( "" );    // Select to ignore this information
     for ( int i = 1; i <= 12; i++ ) {
-        month_Vector.addElement ( "" + i );
+        month_Vector.add( "" + i );
     }
     __month_JComboBox = new SimpleJComboBox ( month_Vector, false );
+    __month_JComboBox.addActionListener ( this );
     __month_JLabel = new JLabel("Month:");
     JGUIUtil.addComponent(this, __month_JLabel,
         x, y_label, 1, 1, 1, 0, insetsTLBR, GridBagConstraints.NONE, GridBagConstraints.WEST);
@@ -292,12 +349,8 @@ private void setupUI ()
         x++, y_entry, 1, 1, 1, 0, insetsTLBR, GridBagConstraints.NONE, GridBagConstraints.WEST);
 
     // Add the day...
-    Vector day_Vector = new Vector();
-    day_Vector.addElement ( "" );    // Select to ignore this information
-    for ( int i = 1; i <= 31; i++ ) {
-        day_Vector.addElement ( "" + i );
-    }
-    __day_JComboBox = new SimpleJComboBox ( day_Vector, false ); // Not editable
+    __day_JComboBox = new SimpleJComboBox ( false ); // Not editable
+    setDayChoices ( __year_JComboBox, __month_JComboBox, __day_JComboBox );
     __day_JLabel = new JLabel("Day:");
     JGUIUtil.addComponent(this, __day_JLabel,
         x, y_label, 1, 1, 1, 0, insetsTLBR, GridBagConstraints.NONE, GridBagConstraints.WEST);
@@ -305,10 +358,10 @@ private void setupUI ()
         x++, y_entry, 1, 1, 1, 0, insetsTLBR, GridBagConstraints.NONE, GridBagConstraints.WEST);
 
     // Add the hour...
-    Vector hour_Vector = new Vector();
-    hour_Vector.addElement ( "" );    // Select to ignore this information
+    List<String> hour_Vector = new Vector();
+    hour_Vector.add ( "" );    // Select to ignore this information
     for ( int i = 0; i <= 23; i++ ) {
-        hour_Vector.addElement ( "" + StringUtil.formatString(i,"%02d") );
+        hour_Vector.add ( "" + StringUtil.formatString(i,"%02d") );
     }
     __hour_JComboBox = new SimpleJComboBox ( hour_Vector, false ); // Not editable
     __hour_JLabel = new JLabel("Hour:");
@@ -318,10 +371,10 @@ private void setupUI ()
         x++, y_entry, 1, 1, 1, 0, insetsTLBR, GridBagConstraints.NONE, GridBagConstraints.WEST);
 
     // Add the minute...
-    Vector minute_Vector = new Vector();
-    minute_Vector.addElement ( "" );    // Select to ignore this information
+    List<String> minute_Vector = new Vector();
+    minute_Vector.add ( "" );    // Select to ignore this information
     for ( int i = 0; i <= 59; i++ ) {
-        minute_Vector.addElement ( "" + StringUtil.formatString(i,"%02d") );
+        minute_Vector.add ( "" + StringUtil.formatString(i,"%02d") );
     }
     __minute_JComboBox = new SimpleJComboBox ( minute_Vector, true );
     __minute_JLabel = new JLabel("Minute:");
@@ -335,6 +388,7 @@ private void setupUI ()
 
 /**
 Set the visibility on the choices consistent with the min/max interval to display.
+For example, don't show the year if year interval is not within the min and max intervals.
  */
 private void setVisibility()
 {
@@ -381,82 +435,159 @@ private void setVisibility()
 }
 
 /**
-Return a string representation of the data in the panel, using ISO formats.
+Return a DateTime (DATE_FAST) representation for the data in the panel.
+The date/time will be initialized to TimeUtil.BLANK values if not specified
+and will have parts set for the enabled fields that are not blank.
+@return a DateTime instance with values set to that of the input components.
 */
-public String toString()
+public DateTime toDateTime ()
 {
-    DateTime d = new DateTime(__interval_min|DateTime.DATE_FAST );
-    // Initialize to all 9s to indicate value not relevant.
-    // These can be checked for in other code.
-    d.setYear( TimeUtil.BLANK_YEAR );
-    d.setMonth ( TimeUtil.BLANK_MONTH );
-    d.setDay ( TimeUtil.BLANK_DAY );
-    d.setHour ( TimeUtil.BLANK_HOUR );
-    d.setMinute ( TimeUtil.BLANK_MINUTE );
-    if ( __year_JComboBox.isVisible() ) {
-        String year = __year_JComboBox.getSelected();
-        if ( TimeUtil.isValidYear(year)) {
-            d.setYear ( StringUtil.atoi(year) );
+    DateTime dt = new DateTime(DateTime.DATE_FAST);
+    if ( __year_JComboBox.isEnabled() ) {
+        String year = __year_JComboBox.getSelected().trim();
+        if ( StringUtil.isInteger(year) ) {
+            dt.setYear(Integer.parseInt(year));
+        }
+        else {
+            dt.setYear ( TimeUtil.BLANK_YEAR );
         }
     }
-    if ( __month_JComboBox.isVisible() ) {
-        String month = __month_JComboBox.getSelected();
-        if ( TimeUtil.isValidMonth(month)) {
-            d.setMonth ( StringUtil.atoi(month) );
+    if ( __month_JComboBox.isEnabled() ) {
+        String month = __month_JComboBox.getSelected().trim();
+        if ( StringUtil.isInteger(month) ) {
+            dt.setMonth(Integer.parseInt(month));
+        }
+        else {
+            dt.setYear ( TimeUtil.BLANK_MONTH );
         }
     }
-    if ( __day_JComboBox.isVisible() ) {
-        String day = __day_JComboBox.getSelected();
-        if ( TimeUtil.isValidDay(day)) {
-            d.setDay ( StringUtil.atoi(day) );
+    if ( __day_JComboBox.isEnabled() ) {
+        String day = __day_JComboBox.getSelected().trim();
+        if ( StringUtil.isInteger(day) ) {
+            dt.setYear(Integer.parseInt(day));
+        }
+        else {
+            dt.setYear ( TimeUtil.BLANK_DAY );
         }
     }
-    if ( __hour_JComboBox.isVisible() ) {
-        String hour = __hour_JComboBox.getSelected();
-        if ( TimeUtil.isValidHour(hour)) {
-            d.setHour ( StringUtil.atoi(hour) );
+    if ( __hour_JComboBox.isEnabled() ) {
+        String hour = __hour_JComboBox.getSelected().trim();
+        if ( StringUtil.isInteger(hour) ) {
+            dt.setYear(Integer.parseInt(hour));
+        }
+        else {
+            dt.setYear ( TimeUtil.BLANK_HOUR );
         }
     }
-    if ( __minute_JComboBox.isVisible() ) {
-        String minute = __minute_JComboBox.getSelected();
-        if ( TimeUtil.isValidMinute(minute)) {
-            d.setMinute ( StringUtil.atoi(minute) );
+    if ( __minute_JComboBox.isEnabled() ) {
+        String minute = __minute_JComboBox.getSelected().trim();
+        if ( StringUtil.isInteger(minute) ) {
+            dt.setYear(Integer.parseInt(minute));
+        }
+        else {
+            dt.setYear ( TimeUtil.BLANK_MINUTE );
         }
     }
-    // Set the precision to ignore blank input, starting from most precise.
-    // This will minimize showing invalid inputs like 99 for hour.
+    return dt;
+}
+
+/**
+Return a string representation of the data in the panel, using ISO format.
+Enabled fields are included, with default (99) values to indicate blanks.
+The year is always included - only parts on the small end are left off if not visible.
+*/
+public String toString ()
+{
+    return toString ( true, false );
+}
+
+// FIXME SAM 2009-11-13 stripping works for hour and minute but not other parts - probably OK
+// for now to keep integrity of the date/time string
+/**
+Return a string representation of the data in the panel, using ISO format.
+Enabled fields are included, with default (99) values to indicate blanks.
+The year is optionally included - only parts on the small end are left off.
+@param includeYear indicate whether the year should be included on front of the string.
+@param stripBlanksFromEnd if true, strip blanks from the end of the string.
+*/
+public String toString ( boolean includeYear, boolean stripBlanksFromEnd )
+{
+    StringBuffer b = new StringBuffer();
+    boolean doDateDelim = false; // Indicates that the delimiter between date parts needs added
+    boolean doTimeDelim = false; // Indicates that the delimiter between time parts needs added
     
-    int [] intervals = {
-            TimeInterval.MINUTE,
-            TimeInterval.HOUR,
-            TimeInterval.DAY,
-            TimeInterval.MONTH,
-            TimeInterval.YEAR
-    };
-    SimpleJComboBox cb = null;
-    for ( int i = 0; i < intervals.length; i++ ) {
-        if ( intervals[i] == TimeInterval.MINUTE ) {
-            cb = __minute_JComboBox;
-        }
-        else if ( intervals[i] == TimeInterval.HOUR ) {
-            cb = __hour_JComboBox;
-        }
-        else if ( intervals[i] == TimeInterval.DAY ) {
-            cb = __day_JComboBox;
-        }
-        else if ( intervals[i] == TimeInterval.MONTH ) {
-            cb = __month_JComboBox;
-        }
-        if ( cb.isVisible() ) {
-            String val = cb.getSelected();
-            if ( val.length() != 0 ) {
-                // Value is specified so assume precision
-                d.setPrecision ( intervals[i] );
-                break;
+    String hour = __hour_JComboBox.getSelected().trim();
+    String minute = __minute_JComboBox.getSelected().trim();
+    
+    if ( includeYear ) {
+        if ( __year_JComboBox.isVisible() ) {
+            String year = __year_JComboBox.getSelected().trim();
+            doDateDelim = true;
+            if ( StringUtil.isInteger(year) ) {
+                b.append ( StringUtil.formatString(Integer.parseInt(year),"%04d"));
+            }
+            else {
+                b.append ( StringUtil.formatString(TimeUtil.BLANK_YEAR,"%04d"));
             }
         }
     }
-    return d.toString();
+    if ( __month_JComboBox.isVisible() ) {
+        String month = __month_JComboBox.getSelected().trim();
+        if ( doDateDelim ) {
+            b.append ( "-" );
+        }
+        else {
+            doDateDelim = true;
+        }
+        if ( StringUtil.isInteger(month) ) {
+            b.append ( StringUtil.formatString(Integer.parseInt(month),"%02d"));
+        }
+        else {
+            b.append ( StringUtil.formatString(TimeUtil.BLANK_MONTH,"%02d"));
+        }
+    }
+    if ( __day_JComboBox.isVisible() ) {
+        String day = __day_JComboBox.getSelected().trim();
+        if ( doDateDelim ) {
+            b.append ( "-" );
+        }
+        if ( StringUtil.isInteger(day) ) {
+            b.append ( StringUtil.formatString(Integer.parseInt(day),"%02d"));
+        }
+        else {
+            b.append ( StringUtil.formatString(TimeUtil.BLANK_DAY,"%02d"));
+        }
+    }
+    if ( __hour_JComboBox.isVisible() ) {
+        if ( doDateDelim ) {
+            b.append ( " " );
+        }
+        doTimeDelim = true;
+        if ( StringUtil.isInteger(hour) ) {
+            b.append ( StringUtil.formatString(Integer.parseInt(hour),"%02d"));
+        }
+        else {
+            if ( !StringUtil.isInteger(minute) && !stripBlanksFromEnd ) {
+                // Need to include the blanks.
+                b.append ( StringUtil.formatString(TimeUtil.BLANK_HOUR,"%02d"));
+            }
+        }
+    }
+    if ( __minute_JComboBox.isVisible() ) {
+        if ( doTimeDelim ) {
+            b.append ( ":" );
+        }
+        if ( StringUtil.isInteger(minute) ) {
+            b.append ( StringUtil.formatString(Integer.parseInt(minute),"%02d"));
+        }
+        else {
+            // Include if the hour is not blank and blanks are to be included.
+            if ( !stripBlanksFromEnd ) {
+                b.append ( StringUtil.formatString(TimeUtil.BLANK_MINUTE,"%02d"));
+            }
+        }
+    }
+    return b.toString();
 }
 
 }
