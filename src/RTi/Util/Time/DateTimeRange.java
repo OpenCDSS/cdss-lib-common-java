614fa467b41a0af726db1d7087fa496444d82081
Steve Malers <steve.malers@openwaterfoundation.org>
2011-04-06 23:14:54 +0000

    Javadoc cleanup.
    git-svn-id: file:///cygdrive/C/owf-svnrepos/RTi_Common/trunk@592 81c4e16f-9576-dd43-849c-dbad87f9bafb

--- src/RTi/Util/Time/DateTimeRange.java
+++ src/RTi/Util/Time/DateTimeRange.java
@@ -3,8 +3,7 @@ package RTi.Util.Time;
 /**
 This class stores a range defined by two DateTime end-points.
 It is useful for specifying a processing period.
-Currently the instance is immutable and copies of the DateTime data are copied
-at construction.
+Currently the instance is immutable and copies of the DateTime data are copied at construction.
 Currently there is no validation done.
 */
 public class DateTimeRange
@@ -21,10 +20,8 @@ public class DateTimeRange
     
     /**
      * Constructor.
-     * @param start starting value in the range.  Can be null to indicate open-ended
-     * range (from available start).
-     * @param end ending value in the range.  Can be null to indicate open-ended
-     * range (to available end).
+     * @param start starting value in the range.  Can be null to indicate open-ended range (from available start).
+     * @param end ending value in the range.  Can be null to indicate open-ended range (to available end).
      */
     public DateTimeRange ( DateTime start, DateTime end )
     {
@@ -34,6 +31,7 @@ public class DateTimeRange
     
     /**
      * Return the ending date/time in the range (can be null) if open-ended.
+     * @return the ending date/time in the range (can be null) if open-ended.
      */
     public DateTime getEnd ()
     {
@@ -42,6 +40,7 @@ public class DateTimeRange
     
     /**
      * Return the starting date/time in the range (can be null) if open-ended.
+     * @return the starting date/time in the range (can be null) if open-ended.
      */
     public DateTime getStart ()
     {
