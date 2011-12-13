b849e346c88f4b132b20d681cc116dafde9bc97f
Steve Malers <steve.malers@openwaterfoundation.org>
2011-12-13 21:24:28 +0000

    Code cleanup.
    git-svn-id: file:///cygdrive/C/owf-svnrepos/RTi_Common/trunk@665 81c4e16f-9576-dd43-849c-dbad87f9bafb

--- src/RTi/Util/Time/DateTimeRange.java
+++ src/RTi/Util/Time/DateTimeRange.java
@@ -3,58 +3,60 @@ package RTi.Util.Time;
 /**
 This class stores a range defined by two DateTime end-points.
 It is useful for specifying a processing period.
+It is NOT the same as DateTimeWindow, which is a window within each year of a period.
 Currently the instance is immutable and copies of the DateTime data are copied at construction.
-Null date/times are allowed.
-Currently there is no validation done.
+Null date/times are allowed.  Currently there is no validation done.
 */
 public class DateTimeRange
 {
+
     /**
-     * Starting DateTime for the range.
-     */
-    private DateTime __start = null;
-    
-    /**
-     * Ending DateTime for the range.
-     */
-    private DateTime __end = null;
-    
-    /**
-     * Constructor.
-     * @param start starting date/time in the range.  Can be null to indicate open-ended range (from available start).
-     * @param end ending date/time in the range.  Can be null to indicate open-ended range (to available end).
-     */
-    public DateTimeRange ( DateTime start, DateTime end )
-    {
-        if ( start == null ) {
-            __start = null;
-        }
-        else {
-            __start = new DateTime ( start );
-        }
-        if ( end == null ) {
-            __end = null;
-        }
-        else {
-            __end = new DateTime ( end );
-        }
+Starting DateTime for the range.
+*/
+private DateTime __start = null;
+
+/**
+Ending DateTime for the range.
+*/
+private DateTime __end = null;
+
+/**
+Constructor.
+@param start starting date/time in the range.  Can be null to indicate open-ended range (from available start).
+@param end ending date/time in the range.  Can be null to indicate open-ended range (to available end).
+*/
+public DateTimeRange ( DateTime start, DateTime end )
+{
+    if ( start == null ) {
+        __start = null;
     }
-    
-    /**
-     * Return the ending date/time in the range (can be null) if open-ended.
-     * @return the ending date/time in the range (can be null) if open-ended.
-     */
-    public DateTime getEnd ()
-    {
-        return __end;
+    else {
+        __start = new DateTime ( start );
     }
-    
-    /**
-     * Return the starting date/time in the range (can be null) if open-ended.
-     * @return the starting date/time in the range (can be null) if open-ended.
-     */
-    public DateTime getStart ()
-    {
-        return __start;
+    if ( end == null ) {
+        __end = null;
     }
+    else {
+        __end = new DateTime ( end );
+    }
+}
+
+/**
+Return the ending date/time in the range (can be null) if open-ended.
+@return the ending date/time in the range (can be null) if open-ended.
+*/
+public DateTime getEnd ()
+{
+    return __end;
+}
+
+/**
+Return the starting date/time in the range (can be null) if open-ended.
+@return the starting date/time in the range (can be null) if open-ended.
+*/
+public DateTime getStart ()
+{
+    return __start;
+}
+
 }
