3046aa9cec67fa2ad219f72feb24196c694529a1
Steve Malers <steve.malers@openwaterfoundation.org>
2013-02-27 22:35:07 +0000

    Change so default is for GenericDatabaseDataStore DMI connection to allow edits.  More granular control can be implemented using normal database authentication.  Write ability is needed with WriteTableToDataStore() and similar commands.
    git-svn-id: file:///cygdrive/C/owf-svnrepos/RTi_Common/trunk@858 81c4e16f-9576-dd43-849c-dbad87f9bafb

--- src/riverside/datastore/GenericDatabaseDataStore.java
+++ src/riverside/datastore/GenericDatabaseDataStore.java
@@ -27,6 +27,9 @@ public GenericDatabaseDataStore ( String name, String description, DMI dmi )
     setName ( name );
     setDescription ( description );
     setDMI ( dmi );
+    // Rely on other authentication to prevent writing.
+    // TODO SAM 2013-02-26 Perhaps use a database configuration file property to control
+    dmi.setEditable ( true );
 }
     
 /**
