8054fcbc20e845e9c16b58d96fe4d0e74b89d28c
Steve Malers <steve.malers@openwaterfoundation.org>
2017-07-14 04:17:11 -0600

    Add a few enhancements to XmlToolkit for use with WaterML 2 parsing.

--- src/RTi/Util/IO/XmlToolkit.java
+++ src/RTi/Util/IO/XmlToolkit.java
@@ -1,15 +1,25 @@
 package RTi.Util.IO;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.w3c.dom.Element;
 import org.w3c.dom.NamedNodeMap;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 
+import RTi.Util.Message.Message;
+
+// TODO smalers 2017-07-12 the methods in this class need to be renamed/refactored.
+// Code was consolidated from several places and the methods are redundant with different naming conventions.
+
 /**
  * Useful XML processing methods as a toolkit to avoid static methods. See: -
  * http://www.drdobbs.com/jvm/easy-dom-parsing-in-java/231002580
+ * 
+ * Element and Node are generally interchangeable for basic functionality and different nomenclature is used
+ * only because of historical reasons.
  */
 public class XmlToolkit {
 
@@ -18,10 +28,97 @@ public class XmlToolkit {
 	 */
 	public XmlToolkit() {
 	}
+	
+	/**
+	Find an element (given a parent element) that matches the given element name.
+	@param parentElement parent element to process
+	@param elementName the element name to match
+	@param attributeName the attribute name to match (if null, don't try to match the attribute name)
+	@return the first matched element, or null if none are matched
+	@throws IOException
+	*/
+	public Element findSingleElement(Element parentElement, String elementName ) throws IOException {
+	    return findSingleElement ( parentElement, elementName, null );
+	}
 
 	/**
-	 * Return the node that matches an element tag name, given a list of nodes
-	 * to search.
+	Find an element (given a starting element) that matches the given element name.
+	@param startElement Starting element to process.
+	@param elementName The element name to match.  Namespace is ignored.
+	@param attributeName Only return the element if the attribute name is matched.
+	@return the first matched element, or null if none are matched
+	@throws IOException
+	*/
+	public Element findSingleElement(Element startElement, String elementName, String attributeName ) throws IOException {
+	    NodeList nodes = startElement.getElementsByTagNameNS("*",elementName);
+	    if ( nodes.getLength() == 0 ) {
+	        return null;
+	    }
+	    else {
+	        if ( (attributeName != null) && !attributeName.equals("") ) {
+	            // Want to search to see if the node has a matching attribute name
+	            for ( int i = 0; i < nodes.getLength(); i++ ) {
+	                Node node = nodes.item(i);
+	                NamedNodeMap nodeMap = node.getAttributes();
+	                if ( nodeMap.getNamedItem(attributeName) != null ) {
+	                    // Found the node of interest
+	                    return (Element)node;
+	                }
+	            }
+	            // No node had the requested attribute
+	            return null;
+	        }
+	        else {
+	            return nodes.getLength() > 0 ? (Element) nodes.item(0) : null;
+	        }
+	    }
+	}
+
+	/**
+	Find an element (given a starting element) that matches the given element name and return its string content.
+	@param startElement parent element to process
+	@param elementName the element name to match, returning the value from the elements getTextContent().
+	@return the first string text of the first matched element, or null if none are matched
+	@throws IOException
+	*/
+	public String findSingleElementValue(Element startElement, String elementName) throws IOException {
+	    Element el = findSingleElement(startElement, elementName);
+	    return el == null ? null : el.getTextContent().trim();
+	}
+	
+	/**
+	Get all elements matching the given name from a starting element
+	@param startElement Starting element to process
+	@param elementName the element name to match, namespace is not used to match.
+	@return the elements matching elementName, or null if not matched
+	@throws IOException if the number of matching nodes is not 1
+	*/
+	public List<Element> getElements(Element startElement, String name) throws IOException {
+	    NodeList nodes = startElement.getElementsByTagNameNS("*",name);
+	    List<Element> elements = new ArrayList<Element>();
+	    for ( int i = 0; i < nodes.getLength(); i++ ) {
+	        elements.add((Element)nodes.item(i));
+	    }
+	    return elements;
+	}
+	
+	/**
+	Get a list of element text values.
+	Return the list of element text values, using values returned from node getTextContent().
+	@param startElement Start element to start the search.
+	@param name Name of element to match.  Namespace is ignored to match elements.
+	*/
+	public List<String> getElementValues(Element startElement, String name) throws IOException {
+	    NodeList nodes = startElement.getElementsByTagNameNS("*",name);
+	    ArrayList<String> vals = new ArrayList<String>(nodes.getLength());
+	    for (int i = 0; i < nodes.getLength(); i++) {
+	        vals.add(((Element) nodes.item(i)).getTextContent().trim());
+	    }
+	    return vals;
+	}
+
+	/**
+	 * Return the node that matches an element tag name, given a list of nodes to search.
 	 * 
 	 * @param tagName
 	 *            element tag name
@@ -41,8 +138,7 @@ public class XmlToolkit {
 	}
 	
 	/**
-	 * Return the nodes that matches an element tag name, given a list of nodes
-	 * to search.
+	 * Return the nodes that matches an element tag name, given a list of nodes to search.
 	 * 
 	 * @param tagName
 	 *            element tag name
@@ -64,18 +160,22 @@ public class XmlToolkit {
 	/**
 	 * Return the attribute value for a node or "" if not found.
 	 * 
-	 * @param attributeName
-	 *            element attribute name
-	 * @param node
-	 *            node to process
+	 * @param attributeName element attribute name, with namespace if present in file
+	 * @param node node to process
 	 * @return string value for attribute name, or "" if not matched
 	 */
 	public String getNodeAttribute(String attributeName, Node node ) {
 	    NamedNodeMap attributes = node.getAttributes();
-	    for (int y = 0; y < attributes.getLength(); y++ ) {
-	        Node attribute = attributes.item(y);
+	    //Message.printStatus(2, "", "Node \"" + node.getNodeName() + "\" has " + attributes.getLength() + " attributes" );
+	    for (int i = 0; i < attributes.getLength(); i++ ) {
+	        Node attribute = attributes.item(i);
+	        //Message.printStatus(2, "", "Comparing requested attribute \"" + attributeName + "\" with element attribute \"" + attribute.getNodeName() +"\"");
 	        if (attribute.getNodeName().equalsIgnoreCase(attributeName)) {
+	        	//Message.printStatus(2, "", "Matched, returning \"" + attribute.getNodeValue() + "\"");
 	            return attribute.getNodeValue();
+	        }
+	        else {
+	        	//Message.printStatus(2, "", "Not Matched" );
 	        }
 	    }
 	    return "";
@@ -104,4 +204,54 @@ public class XmlToolkit {
 	    }
 	    return "";
 	}
+	
+	/**
+	Return the text value of the single matching element.
+	@param parentElement Starting element to process
+	@param elementName the element name to match, namespace is not used to match.
+	@return the first string text of the first matched element (from getTextContent), or null if none are matched
+	@throws IOException
+	*/
+	public String getSingleElementValue(Element startElement, String elementName)
+	throws IOException {
+	    return getSingleElement(startElement, elementName).getTextContent().trim();
+	}
+
+	/**
+	Get the single element matching the given name from a starting element
+	@param startElement Starting element to process
+	@param elementName the element name to match, namespace is not used to match.
+	@return the element matching elementName, or null if not matched
+	@throws IOException if the number of matching nodes is not 1
+	*/
+	public Element getSingleElement(Element startElement, String name) throws IOException {
+	    NodeList nodes = startElement.getElementsByTagNameNS("*",name);
+	    if (nodes.getLength() != 1) {
+	        throw new IOException("Expected to find 1 child \"" + name + "\" in \"" + startElement.getTagName() + "\" - found " + nodes.getLength() );
+	    }
+	    return (Element) nodes.item(0);
+	}
+	
+	/**
+	Get the single element matching the given name moving up in the DOM relative to the starting element.
+	@param startElement Starting element to process
+	@param elementName the element name to match, namespace is not used to match.
+	@return the element matching elementName, or null if not matched
+	*/
+	public Element getSingleElementPrevious(Element startElement, String elementName) throws IOException {
+		Node node = startElement;
+		while ( true ) {
+			// Get the parent node
+			node = node.getParentNode();
+			if ( node == null ) {
+				// Did not find the requested node
+				break;
+			}
+			if ( node.getNodeName().equalsIgnoreCase(elementName) ) {
+				return (Element)node;
+			}
+		}
+	    return null;
+	}
+
 }
